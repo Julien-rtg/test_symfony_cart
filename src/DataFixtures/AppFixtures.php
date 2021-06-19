c3861883e027b6ac10d36e97c765eb0d0b14f92e
Julien-rtg <julien.rittl@gmail.com>
2021-06-19 14:12:26 +0200

    feature cart rdyToUse

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,26 +3,22 @@
 namespace App\DataFixtures;
 
 use App\Entity\Product;
+use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $faker = \Faker\Factory::create();
-        $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
-        $faker->addProvider(new \Liior\Faker\Prices($faker));
+        for($i = 1; $i <= 15; $i++){
 
-        for ($i = 0; $i < 10; $i++) {
             $product = new Product();
-            $product->setTitle($faker->productName)
-                ->setPrice($faker->price(20, 200))
-                ->setImage($faker->imageUrl(400, 400));
-
+            $product->settitle("Product$i");
+            $product->setimage("https://via.placeholder.com/300");
+            $product->setprice($i*rand(2, 10));
             $manager->persist($product);
+    
+            $manager->flush();
         }
-
-        $manager->flush();
     }
 }
